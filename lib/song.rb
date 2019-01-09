5d5491a94adc96f1d86b7d996f4365068acf2410
davidfloyd91 <davidfloyd91@gmail.com>
2019-01-09 18:22:30 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+require_relative './artist'
+
+class Song
+
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name=nil, artist=nil)
+    @name = name
+    @artist = artist
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+end
