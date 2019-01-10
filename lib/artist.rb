03364298a69cd1669341c6d696c9cf72f2f25ee2
github username <github email address>
2019-01-09 20:25:50 -0500

    updated

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,15 +3,8 @@ require 'pry'
 class Artist
   attr_accessor :name
 
-  @@all = []
-
   def initialize(name)
     @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
   end
 
   def songs
